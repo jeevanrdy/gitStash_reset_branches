2280fb7c35853860ba3773d5d5773f881e1697fd
Jeevan Reddy <59163883+jeevanrdy@users.noreply.github.com>
2024-06-19 16:33:02 +0530

    Add files via upload

---
+++ gitclass2/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SRH 2024</h1>
+      <img src="./srhimages/srh.webp" alt="teamimage"></img>
+      <h3>Key Players: Pat Cummins, Travis Head, Abhishek Sharma.</h3>
+    </div>
+  );
+}
+
+export default App;
